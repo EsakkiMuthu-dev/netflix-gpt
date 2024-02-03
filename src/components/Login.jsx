96041697a9b14b4527980fa3a9261297cdbf6017
Esakki Muthu <esakkimuthu6842@gmail.com>
2024-02-03 21:08:40 +0530

    intial  step completed

--- src/components/Login.jsx
+++ src/components/Login.jsx
@@ -1,21 +1,35 @@
-import { useState } from 'react';
+import { useRef, useState } from 'react';
 import Header from './Header';
+import { validateData } from '../utils/validate';
 
 const Login = () => {
   const [isSignIn, setIsSignIn] = useState(true);
+  const [errorMessage, setErrorMessage] = useState(null);
+
+  const email = useRef(null);
+  const password = useRef(null);
+
+  const validateForm = () => {
+    // validate data
+    // console.log(email.current.value);
+    // console.log(password.current.value);
+    const message = validateData(email.current.value, password.current.value);
+    setErrorMessage(message);
+  };
   const toggleForm = () => {
     setIsSignIn(!isSignIn);
   };
   return (
     <>
-      <Header className="absolute " />
+      <Header className="absolute" />
       <div>
         <img
-          className=" absolute h-full "
+          className="absolute  bg-fixed "
           src="https://assets.nflxext.com/ffe/siteui/vlv3/4da5d2b1-1b22-498d-90c0-4d86701dffcc/98a1cb1e-5a1d-4b98-a46f-995272b632dd/IN-en-20240129-popsignuptwoweeks-perspective_alpha_website_large.jpg"
           alt="background"
         />
         <form
+          onSubmit={(e) => e.preventDefault()}
           className="absolute mt-40 mx-auto left-0 right-0 w-1/4 p-10
          bg-black text-white flex flex-wrap bg-opacity-80 rounded-lg">
           <h1 className="font-bold text-2xl ">{isSignIn ? ' Sign In ' : ' Sign Up '}</h1>
@@ -27,16 +41,22 @@ const Login = () => {
             />
           )}
           <input
+            ref={email}
             type="email"
             placeholder=" Email Address"
             className=" px-3 py-2 mx-4  bg-gray-500 my-4 w-full"
           />
           <input
+            ref={password}
             type="text "
             placeholder=" Password "
             className=" px-3 py-2 mx-4 bg-gray-500  my-4 w-full"
           />
-          <button type="submit" className="px-3 py-2 mx-4 my-4  bg-red-800 w-full rounded-lg">
+          <p className="px-3 py-2 font-bold text-lg text-red-500">{errorMessage}</p>
+          <button
+            type="submit"
+            onClick={validateForm}
+            className="px-3 py-2 mx-4 my-4  bg-red-800 w-full rounded-lg">
             {isSignIn ? ' Sign In ' : ' Sign Up '}
           </button>
 
