8c6d9fc428e61b42547d462a74e30c0bf1bb6169
Esakki Muthu <esakkimuthu6842@gmail.com>
2024-02-04 22:38:21 +0530

    Update Browse.jsx

--- src/components/Browse.jsx
+++ src/components/Browse.jsx
@@ -1,7 +1,7 @@
 const Browse = () => {
   return (
     <div>
-      <h1 className=" text-center font-bold text-2xl">Browse</h1>
+      <h1 className=" text-center font-bold text-2xl"> Browse Movies</h1>
     </div>
   );
 };
