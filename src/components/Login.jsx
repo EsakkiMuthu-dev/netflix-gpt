be1626070b00dd6d0e1062e743e54b893ae52f0e
Esakki Muthu <esakkimuthu6842@gmail.com>
2024-02-05 21:52:54 +0530

    user authentication added

--- src/components/Login.jsx
+++ src/components/Login.jsx
@@ -1,20 +1,55 @@
 import { useRef, useState } from 'react';
 import Header from './Header';
 import { validateData } from '../utils/validate';
-
+import { createUserWithEmailAndPassword, signInWithEmailAndPassword } from 'firebase/auth';
+import { auth } from '../utils/firebase';
 const Login = () => {
   const [isSignIn, setIsSignIn] = useState(true);
   const [errorMessage, setErrorMessage] = useState(null);
 
   const email = useRef(null);
+  const name = useRef(null);
   const password = useRef(null);
 
   const validateForm = () => {
     // validate data
     // console.log(email.current.value);
     // console.log(password.current.value);
+    console.log('sign up / sign in?');
+    console.log(`is sign in - ${isSignIn}`);
     const message = validateData(email.current.value, password.current.value);
     setErrorMessage(message);
+    if (message) return;
+
+    //signup and sign in
+    if (!isSignIn) {
+      console.log('Going to sign up');
+      //create user in firebase
+      createUserWithEmailAndPassword(auth, email.current.value, password.current.value)
+        .then((userCredential) => {
+          // Signed up
+          const user = userCredential.user;
+          console.log(user);
+        })
+        .catch((error) => {
+          const errorCode = error.code;
+          const errorMessage = error.message;
+          setErrorMessage(`${errorCode} : ${errorMessage}`);
+        });
+    } else {
+      signInWithEmailAndPassword(auth, email.current.value, password.current.value)
+        .then((userCredential) => {
+          // Signed in
+          const user = userCredential.user;
+          console.log(user);
+          // after successful sign in i have to go to Browse page
+        })
+        .catch((error) => {
+          const errorCode = error.code;
+          const errorMessage = error.message;
+          setErrorMessage(`${errorCode} - ${errorMessage}`);
+        });
+    }
   };
   const toggleForm = () => {
     setIsSignIn(!isSignIn);
@@ -35,6 +70,7 @@ const Login = () => {
           <h1 className="font-bold text-2xl ">{isSignIn ? ' Sign In ' : ' Sign Up '}</h1>
           {!isSignIn && (
             <input
+              ref={name}
               type="text"
               placeholder="Full Name"
               className=" px-3 py-2 mx-4  bg-gray-500 my-4 w-full"
@@ -48,7 +84,7 @@ const Login = () => {
           />
           <input
             ref={password}
-            type="text "
+            type="password"
             placeholder=" Password "
             className=" px-3 py-2 mx-4 bg-gray-500  my-4 w-full"
           />
